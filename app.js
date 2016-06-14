372925e34eb3bb6565630a7d046947c32bd5313f
Lucas Louca <lucaslouca@users.noreply.github.com>
2016-06-14 10:01:39 +0200

    Create app.js

---
+++ app.js
@@ -0,0 +1,8 @@
+angular.module('MyApp', ['ngTokenField']).controller('NewSurveyCtrl', function($scope, AuthService, AUTH_EVENTS) {
+	$scope.validator = function(text) {
+		var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+		return re.test(text);
+    };
+	
+	$scope.tags = [];
+});
